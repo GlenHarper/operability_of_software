62cc3b09e40ae73fd346f453ec4628fb10942ea2
Heidar Bernhardsson <heidarbn@gmail.com>
2019-01-04 11:43:02 +0000

    add custom exception and log message

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,8 @@
 class WelcomeController < ApplicationController
   def index
     if Random.rand(10) == 5
-      raise StandardError, 'There is a 10% chance this route errors'
+      logger.debug 'There is a 10% chance this action raises an exception'
+      raise RandomError, 'There is a 10% chance this route errors'
     end
   end
 end
---
+++ app/errors/random_error.rb
@@ -0,0 +1,2 @@
+class RandomError < StandardError
+end
