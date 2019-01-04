ceff6accb0150cd32a8522b4409e5c09a0d6ccba
Heidar Bernhardsson <heidarbn@gmail.com>
2019-01-04 11:14:13 +0000

    add log message

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-    raise Exception if Random.rand(10) == 5
+    if Random.rand(10) == 5
+      logger.debug 'There is a 10% chance this route errors'
+      raise StandardError, 'There is a chance this route errors'
+    end
   end
 end
