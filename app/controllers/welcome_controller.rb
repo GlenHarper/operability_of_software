b57bebdcb274501681c6b31b4bfd55ccf2b29cf3
Heidar Bernhardsson <heidarbn@gmail.com>
2019-01-04 11:15:44 +0000

    remove logging
    The exception gets written to the log anyway.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     if Random.rand(10) == 5
-      logger.debug 'There is a 10% chance this route errors'
-      raise StandardError, 'There is a chance this route errors'
+      raise StandardError, 'There is a 10% chance this route errors'
     end
   end
 end
