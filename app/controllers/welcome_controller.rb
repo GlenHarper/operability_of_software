69070123c69b452f78683b67f943c33b2898adf2
Heidar Bernhardsson <heidarbn@gmail.com>
2019-01-04 12:06:10 +0000

    remove percentage chance from exception message

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   def index
     if Random.rand(10) == 5
       logger.debug 'There is a 10% chance this action raises an exception'
-      raise RandomError, 'There is a 10% chance this route errors'
+      raise RandomError, 'There is a chance this route errors'
     end
   end
 end
